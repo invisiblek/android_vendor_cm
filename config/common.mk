ffdf031cf67b6aaf932d645434526316a0a581b1
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-15 10:34:35 -0600

    cm: remove ro.adb.secure=1
    Change-Id: I0fcc98377e5e755bef42822790f61f62a828eca8

--- config/common.mk
+++ config/common.mk
@@ -68,11 +68,6 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-endif
-
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
